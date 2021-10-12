e08f7a512423ce7926e002747dc4c79d9598ba41
Khadiza-Aurthee <90924586+Khadiza-Aurthee@users.noreply.github.com>
2021-10-12 12:30:05 -0400

    Changes to App.js

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 
 function App() {
   return (
+
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <h1>About Khadiza</h1>
+          I like birds.
         </p>
         <a
           className="App-link"
